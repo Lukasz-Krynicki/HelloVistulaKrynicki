a78ab48f5473c6a879c65c75cba245144385109b
Lukasz Krynicki <you@example.com>
2022-10-06 17:33:34 +0200

    TESTOWY COMMIT

---
+++ src/pl/vistula/helloworldKrynicki/HelloWorldKrynicki.java
@@ -0,0 +1,10 @@
+package pl.vistula.helloworldKrynicki;
+
+public class HelloWorldKrynicki {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula Krynicki");
+        System.out.println("Hello Vistula Krynicki");
+        System.out.println("TEST TEST TEST");
+        System.out.println("WOWWWWWWWWWW");
+    }
+}
